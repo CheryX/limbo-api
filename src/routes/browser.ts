1910f5a550f3f468d234315036b63512791725b5
Monika Aki <monika.aki@proton.me>
2026-02-08 13:47:51 +0100

    Update /browser/ to new schema

--- src/routes/browser.ts
+++ src/routes/browser.ts
@@ -3,39 +3,52 @@
 // DELETE /browser/:dir - Delete a directory
 // PUT /browser/:dir?new_name= - Rename a directory
 
-import express, { Router } from 'express'
+import express, { Router } from 'express';
 import query from '../lib/db';
-export const router: Router = express.Router()
+import { randomUUID } from 'crypto';
+import { UsosUser } from '../lib/passport';
+
+export const router: Router = express.Router();
 
 router.get('/:dir', async (req, res) => {
     const dir = req.params.dir;
-    const contents = await query("SELECT * FROM files WHERE filepath LIKE $1;", [`${dir}/%`]);
-
-    res.json( contents );
+    const contents = await query("SELECT * FROM files WHERE path = $1;", [dir]);
+    res.json(contents);
 });
 
-// Assume that an empty directory contains a placeholder file named '.placeholder'
 router.post('/:dir', async (req, res) => {
     const dir = req.params.dir;
-    await query("INSERT INTO files (filepath) VALUES ($1);", [`${dir}/.placeholder`]);
+    const key = randomUUID();
+    const userId = (req.user as UsosUser).id;
 
-    res.json({ message: `Created directory: ${dir}` });
+    await query(
+        "INSERT INTO files (key, user_id, name, size, path, visible) VALUES ($1, $2, $3, $4, $5, $6);",
+        [key, userId, '.placeholder', 0, dir, 1]
+    );
+
+    res.status(201).json({ message: `Created directory: ${dir}`, key });
 });
 
 router.delete('/:dir', async (req, res) => {
-    const dir = req.params.dir;
-    await query("DELETE FROM files WHERE filepath LIKE $1;", [`${dir}/%`]);
+    const dir = req.params.dir;    
+    await query("DELETE FROM files WHERE path = $1 OR path LIKE $2;", [dir, `${dir}/%`]);
 
     res.json({ message: `Deleted directory: ${dir}` });
 });
 
 router.put('/:dir', async (req, res) => {
-    const dir = req.params.dir;
-    const newName = String(req.query.new_name);
+    const oldPath = req.params.dir;
+    const newPath = req.query.new_name as string;
 
-    await query("UPDATE files SET filepath = REPLACE(filepath, $1, $2) WHERE filepath LIKE $3;", [dir, newName, `${dir}/%`]);
+    if (!newPath)
+        return res.status(400).json({ error: "Query parameter 'new_name' is required" });
 
-    res.json({ message: `Renamed directory: ${dir} to ${newName}` });
+    await query(
+        "UPDATE files SET path = regexp_replace(path, $1, $2) WHERE path = $3 OR path LIKE $4;",
+        [`^${oldPath}`, newPath, oldPath, `${oldPath}/%`]
+    );
+
+    res.json({ message: `Renamed directory: ${oldPath} to ${newPath}` });
 });
 
 export default router;
